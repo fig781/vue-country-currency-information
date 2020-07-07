a12a87cebe0f8c9dc607636f9264c2d83e340f18
Aden Eilers <adeneilers@gmail.com>
2020-07-06 18:04:20 -0700

    UI change

---
+++ src/components/Search.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="search">
+    <b-container>
+      <b-row class="row-1">
+        <b-col></b-col>
+        <b-col>
+          <b-form @add="onAdd" @submit="onSubmit" @clear="onClear">
+            
+            <b-input
+              id="search-box"
+              type="search"
+              required
+              placeholder="ex: Brazil"
+            ></b-input>
+            
+            <b-button type="add" variant="primary">Add</b-button>
+            <b-button type="submit" variant="success">Submit</b-button>
+            <b-button type="reset" variant="danger">Clear</b-button>
+
+          </b-form>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+
+      <b-row class="row-2">
+        <b-col></b-col>
+        <b-col-7>
+          <selections></selections>
+          <selections></selections>
+          
+        </b-col-7>
+        <b-col></b-col>
+      </b-row>
+      
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+import Selections from './Selections'
+export default {
+  components:{
+    'selections': Selections
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.row-1{
+  text-align: center;
+}
+.row-2{
+
+}
+
+</style>
---
+++ src/components/Selections.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="selection">
+    sdkfjgl;sdk
+  <button>
+    <BIconXCircle variant="light"></BIconXCircle>
+  </button>
+  </div>
+</template>
+
+<script>
+import { BIconXCircle } from 'bootstrap-vue'
+
+export default{
+  components: {
+    BIconXCircle
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.selection{
+  display:inline-block;
+  padding:6px;
+  background-color:slategray;
+  color:white;
+  border-radius: 25px;
+}
+button{
+  box-shadow: none;
+  border: none;
+  text-shadow: none;
+  background-color: transparent;
+  outline: none;
+}
+#x-button{
+  
+}
+
+</style>
