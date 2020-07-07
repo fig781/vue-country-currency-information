1f09cd6de6966b3285f926b42a5450a17e180f4a
Aden Eilers <adeneilers@gmail.com>
2020-07-06 18:05:25 -0700

    extra changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
+<!--All components get routed into here-->
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-row id="headers">
+        <b-col > 
+          <h1>Forex Currency Information</h1>
+          <h3>Search for a country to get started</h3>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container>
+      <b-row >
+        <b-col>
+          <search></search>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import all components in here
+//import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'search': Search
+    
   }
 }
 </script>
 
 <style>
+ /* Global styles here */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+#headers{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
